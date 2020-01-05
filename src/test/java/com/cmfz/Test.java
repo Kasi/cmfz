9bc2f578e15c3a5120984e0a160135e372317af1
your Name <your@Name>
2020-01-05 09:17:26 +0800

    v5.0

--- src/test/java/com/cmfz/Test.java
+++ src/test/java/com/cmfz/Test.java
@@ -22,7 +22,7 @@ public class Test {
     public static void main(String[] args) {
         //GitHub
         String s = DigestUtils.md5Hex("123456a54121f4-2c44-4f06-b732-aa2160cc3e8d");
-
+        System.out.println(s);
         System.out.println(s);
 
         System.out.println(6666);
